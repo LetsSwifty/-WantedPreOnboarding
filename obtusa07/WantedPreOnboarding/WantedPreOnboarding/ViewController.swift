debcefb3afe6b26352810f210fddf443c4d7be49
Taehwan Kim <obtusa07@gmail.com>
2023-02-23 17:36:52 +0900

    :tada: Init Project

---
+++ obtusa07/WantedPreOnboarding/WantedPreOnboarding/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WantedPreOnboarding
+//
+//  Created by Taehwan Kim on 2023/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
