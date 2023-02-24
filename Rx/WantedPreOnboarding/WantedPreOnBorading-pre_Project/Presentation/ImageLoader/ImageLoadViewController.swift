8e793d616eff7d778fb8cc2e0249beeed7e260ff
KANG DONGYEONG <50406861+kangddong@users.noreply.github.com>
2023-02-24 18:17:24 +0900

    Add Logic

--- Rx/WantedPreOnboarding/WantedPreOnBorading-pre_Project/Presentation/ImageLoader/ImageLoadViewController.swift
+++ Rx/WantedPreOnboarding/WantedPreOnBorading-pre_Project/Presentation/ImageLoader/ImageLoadViewController.swift
@@ -9,11 +9,62 @@ import UIKit
 
 class ImageLoadViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var loadAllButton: UIButton!
+    
+    private var imageList: [UIImage?] = []
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        configureTableView()
+        addAction()
+    }
+    
+    private func configureTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.rowHeight = UITableView.automaticDimension
+    }
+    
+    private func addAction() {
+        loadAllButton.addTarget(self, action: #Selector(loadAllImage), for: .touchUpInside)
+    }
+    
+    @objc
+    private func loadAllImage() {
+    }
+
+}
+
+extension ImageLoadViewController: UITalbeViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return imageList.count   
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: ImageListCell.identifier, for: indexPath)
+        guard let convertedCell = cell as? ImageListCell else { return cell }
+//        convertedCell.setImage()
+        
+        return convertedCell
+    }
+}
+class ImageListCell: UITableViewCell {
+
+    static let identifier: String = String(describing: ImageListCell.self)
+    
+    @IBOutlet weak var contentImageView: UIImageView!
+    @IBOutlet weak var loadButton: UIButton!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+             
+    }
+    
+    public func setImage(_ image: UIImage?) {
+        
     }
 
 
 }
-
