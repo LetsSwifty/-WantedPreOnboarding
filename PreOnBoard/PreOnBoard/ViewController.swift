e25345bb28bf10f9aea54bc9baf49ea7b82a4b88
Grizzly <dydrhks02@naver.com>
2023-02-24 14:02:46 +0900

    [feat] Project Start

---
+++ PreOnBoard/PreOnBoard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PreOnBoard
+//
+//  Created by 임 용관 on 2023/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
