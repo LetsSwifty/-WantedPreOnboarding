46c88ed9d607ffae4f6cb0f2387806dcd683f1fc
Taehwan Kim <obtusa07@gmail.com>
2023-02-26 15:14:43 +0900

    :sparkles: 순서 없는 allLoad 기능까지 완성

--- obtusa07/WantedPreOnboarding/WantedPreOnboarding/ViewController.swift
+++ obtusa07/WantedPreOnboarding/WantedPreOnboarding/ViewController.swift
@@ -7,13 +7,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
     
     @IBOutlet weak var imageViewFive: UIImageView!
     @IBOutlet weak var imageViewFour: UIImageView!
     @IBOutlet weak var imageViewThree: UIImageView!
     @IBOutlet weak var imageViewTwo: UIImageView!
     @IBOutlet weak var imageViewOne: UIImageView!
+    
     @IBOutlet weak var buttonFive: UIButton!
     @IBOutlet weak var buttonFour: UIButton!
     @IBOutlet weak var buttonThree: UIButton!
@@ -21,28 +22,157 @@ class ViewController: UIViewController {
     @IBOutlet weak var buttonOne: UIButton!
     @IBOutlet weak var buttonAll: UIButton!
     
+    private let operationQueue = OperationQueue()
+    private var imageDatas: [Data] = []
     override func viewDidLoad() {
         super.viewDidLoad()
-        // 버튼 기능 정의 각 버튼이 하나의 operation 작동하도록
+        imageTag()
     }
     
+    private func imageTag() {
+        imageViewFive.tag = 5
+        imageViewFour.tag = 4
+        imageViewThree.tag = 3
+        imageViewTwo.tag = 2
+        imageViewOne.tag = 1
+    }
+    
+    
+    
     @IBAction func buttonFiveTapped(_ sender: Any) {
-        
+        DispatchQueue.main.async {
+            self.imageViewFive.image = UIImage(systemName: "photo")
+        }
+        operationQueue.addOperation {
+            let viewModel = ViewModel(imageURL: WPModel.fiveImageUrl)
+            viewModel.fetchImage { [weak self] result in
+                switch result {
+                case .success(let data):
+                    DispatchQueue.main.async {
+                        let image = UIImage(data: data)
+                        self?.imageViewFive.image = image
+                    }
+                case .failure(let error):
+                    print(String(describing: error))
+                    break
+                }
+            }
+        }
     }
     
     @IBAction func buttonFourTapped(_ sender: Any) {
+        DispatchQueue.main.async {
+            self.imageViewFour.image = UIImage(systemName: "photo")
+        }
+
+        operationQueue.addOperation {
+            let viewModel = ViewModel(imageURL: WPModel.fourImageUrl)
+            viewModel.fetchImage { [weak self] result in
+                switch result {
+                case .success(let data):
+                    DispatchQueue.main.async {
+                        let image = UIImage(data: data)
+                        self?.imageViewFour.image = image
+                    }
+                case .failure(let error):
+                    print(String(describing: error))
+                    break
+                }
+            }
+        }
+
     }
 
     @IBAction func buttonThreeTapped(_ sender: Any) {
+        DispatchQueue.main.async {
+            self.imageViewThree.image = UIImage(systemName: "photo")
+        }
+        operationQueue.addOperation {
+            let viewModel = ViewModel(imageURL: WPModel.threeImageUrl)
+            viewModel.fetchImage { [weak self] result in
+                switch result {
+                case .success(let data):
+                    DispatchQueue.main.async {
+                        let image = UIImage(data: data)
+                        self?.imageViewThree.image = image
+                    }
+                case .failure(let error):
+                    print(String(describing: error))
+                    break
+                }
+            }
+        }
     }
 
     @IBAction func buttonTwoTapped(_ sender: Any) {
+        DispatchQueue.main.async {
+            self.imageViewTwo.image = UIImage(systemName: "photo")
+        }
+        operationQueue.addOperation {
+            let viewModel = ViewModel(imageURL: WPModel.twoImageUrl)
+            viewModel.fetchImage { [weak self] result in
+                switch result {
+                case .success(let data):
+                    DispatchQueue.main.async {
+                        let image = UIImage(data: data)
+                        self?.imageViewTwo.image = image
+                    }
+                case .failure(let error):
+                    print(String(describing: error))
+                    break
+                }
+            }
+        }
     }
     
     @IBAction func buttonOneTapped(_ sender: Any) {
+        DispatchQueue.main.async {
+            self.imageViewOne.image = UIImage(systemName: "photo")
+        }
+
+        operationQueue.addOperation {
+            let viewModel = ViewModel(imageURL: WPModel.oneImageUrl)
+            viewModel.fetchImage { [weak self] result in
+                switch result {
+                case .success(let data):
+                    DispatchQueue.main.async {
+                        let image = UIImage(data: data)
+                        self?.imageViewOne.image = image
+                    }
+                case .failure(let error):
+                    print(String(describing: error))
+                    break
+                }
+            }
+        }
     }
     
     @IBAction func buttonAllTapped(_ sender: Any) {
+        DispatchQueue.main.async {
+            self.imageViewFive.image = UIImage(systemName: "photo")
+            self.imageViewFour.image = UIImage(systemName: "photo")
+            self.imageViewThree.image = UIImage(systemName: "photo")
+            self.imageViewTwo.image = UIImage(systemName: "photo")
+            self.imageViewOne.image = UIImage(systemName: "photo")
+        }
+        let urls = [WPModel.oneImageUrl, WPModel.twoImageUrl, WPModel.threeImageUrl, WPModel.fourImageUrl, WPModel.fiveImageUrl]
+        
+        for (index, url) in urls.enumerated() {
+            let imageView = self.view.viewWithTag(index + 1) as! UIImageView
+            let viewModel = ViewModel(imageURL: url)
+            viewModel.fetchImage { result in
+                switch result {
+                case .success(let data):
+                    DispatchQueue.main.sync {
+                            let image = UIImage(data: data)
+                            imageView.image = image
+                    }
+                case .failure(let error):
+                    print(String(describing: error))
+                    break
+                }
+            }
+        }
     }
 }
 
